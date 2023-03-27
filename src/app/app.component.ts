de18dc96edb292d3dd0f144f28050744225534a1
bartek <smalecbartlomiej@gmail.com>
2023-03-27 19:17:22 +0200

    Added forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prime-ng-demo';
+  items: MenuItem[];
+  sidebarVisible: boolean;
+  value: string;
+  selectedCities: string[];
+  passwordStrength = 0;
+  checked_one: boolean = false;
+  checked_two: boolean = false;
+  passwordStrengthText = '';
+
+
+  constructor() {
+    this.items = [];
+    this.sidebarVisible = false;
+    this.value = "";
+    this.selectedCities = [];
+  }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Update',
+        icon: 'pi pi-refresh',
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-times',
+      },
+      {
+        label: 'Angular',
+        icon: 'pi pi-external-link',
+        url: 'http://angular.io',
+      },
+      {
+        label: 'Router',
+        icon: 'pi pi-upload',
+        routerLink: '/fileupload',
+      },
+    ];
+  }
+
+  resolved(captchaResponse: string) {
+    console.log(`Resolved captcha with response: ${captchaResponse}`);
+  }
+
+  checkPasswordStrength(event: Event) {
+    const password = (event.target as HTMLInputElement).value;
+    let strength = 0;
+    const regex = [
+      /[a-z]/,
+      /[A-Z]/,
+      /[0-9]/,
+      /[^A-Za-z0-9]/
+    ];
+
+    regex.forEach((pattern) => {
+      if (pattern.test(password)) {
+        strength++;
+      }
+    });
+
+    this.passwordStrength = (strength / regex.length) * 100;
+
+    if (this.passwordStrength >= 76) {
+      this.passwordStrengthText = 'Very strong';
+    } else if (this.passwordStrength >= 51) {
+      this.passwordStrengthText = 'Strong';
+    } else if (this.passwordStrength >= 31) {
+      this.passwordStrengthText = 'Medium';
+    } else {
+      this.passwordStrengthText = 'Very weak';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from "@angular/forms"
+import { DividerModule } from 'primeng/divider';
+import { RippleModule } from 'primeng/ripple';
+import { TabViewModule } from 'primeng/tabview';
+import { CheckboxModule } from 'primeng/checkbox';
+import { SliderModule } from 'primeng/slider';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
+import { ProgressBarModule } from 'primeng/progressbar';
+// For dynamic progressbar demo
+import { ToastModule } from 'primeng/toast';
+
+
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ProgressBarModule,
+    ToastModule,
+    CheckboxModule,
+    FormsModule,
+    InputSwitchModule,
+    TabViewModule,
+    PasswordModule,
+    SliderModule,
+    ButtonModule,
+    CardModule,
+    DividerModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    RippleModule,
+    SplitButtonModule,
+    SidebarModule,
+    InputTextModule,
+    AppRoutingModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
